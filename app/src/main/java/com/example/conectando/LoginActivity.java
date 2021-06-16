4cada7387784dd2d6704f21e043d1648372bb735
JesusHernandezJimenez <jesushernandezjimenez1998@gmail.com>
2021-06-16 17:51:26 -0500

    Toques finales 2

--- app/src/main/java/com/example/conectando/LoginActivity.java
+++ app/src/main/java/com/example/conectando/LoginActivity.java
@@ -5,17 +5,70 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText edUser, edPass;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+        edUser = findViewById(R.id.user);
+        edPass = findViewById(R.id.pass);
+        btnLogin = findViewById(R.id.iniciar);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarSesion("http://192.168.1.67/crud2/validarUsuario.php");
+            }
+        });
     }
 
-    public void iniciarSesion(View view){
-        Intent sig = new Intent(this, MainActivity.class);
-        startActivity(sig);
+    public void iniciarSesion(String URL){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(!response.isEmpty()){
+                    Intent sig = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(sig);
+                }else{
+                    Toast.makeText(LoginActivity.this, "Usuario o contraseña incorrecta", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+                Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError{
+                Map<String, String> parametros= new HashMap<String, String>();
+                parametros.put("usuario", edUser.getText().toString());
+                parametros.put("contrasena", edPass.getText().toString());
+
+
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
     }
 }

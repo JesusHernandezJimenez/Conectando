ef6b5f9162737abb41fac0b293bbe3375779beeb
JesusHernandezJimenez <jesushernandezjimenez1998@gmail.com>
2021-06-18 18:01:05 -0500

    Devices 2

--- app/src/main/java/com/example/conectando/Devices.java
+++ app/src/main/java/com/example/conectando/Devices.java
@@ -3,12 +3,42 @@ package com.example.conectando;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class Devices extends AppCompatActivity {
 
+    TextView txtEdit, band;
+    Switch aSwitch;
+    LinearLayout dis;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_devices);
+        txtEdit = findViewById(R.id.editSelec);
+        aSwitch = findViewById(R.id.switch3);
+        dis = findViewById(R.id.layoutDis);
+        band = findViewById(R.id.band);
+    }
+
+
+    public void onClick(View view) {
+        if(view.getId() == R.id.switch3){
+            if(aSwitch.isChecked()){
+                txtEdit.setText("Conectado");
+            }else{
+                txtEdit.setText("No conectado");
+            }
+        }
+    }
+
+    public void encender(View view) {
+        if(band.getText() == "Encender"){
+            band.setText("Apagar");
+        }else{
+            band.setText("Encender");
+        }
     }
 }
